709b4b6c22f7e23ec316e9d079a8a38471b3ebc9
Yana Duran <kirstine.duran22@gmail.com>
2025-02-24 10:33:37 +1100

    Initial Commit

---
+++ Fruitfull/FruitfullApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitfullApp.swift
+//  Fruitfull
+//
+//  Created by Yana Duran on 24/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct FruitfullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

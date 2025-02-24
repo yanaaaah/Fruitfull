5929eb2628d640df67e57a38cde0dde0acc5f306
Tilly <tenicoll02@gmail.com>
2025-02-25 10:26:13 +1100

    this is the logging page

---
+++ Fruitfull/EditGoalsBubbleSubView.swift
@@ -0,0 +1,18 @@
+//
+//  EditGoalsBubbleSubView.swift
+//  Fruitfull
+//
+//  Created by Tilly Nicoll on 25/2/2025.
+//
+
+import SwiftUI
+
+struct EditGoalsBubbleSubView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditGoalsBubbleSubView()
+}
